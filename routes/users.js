4dae699adeb98fb31a26df78a88bddc61db5312a
Prajval Raval <ravalprajval@gmail.com>
2019-07-18 22:03:32 +0530

    stable v1

--- routes/users.js
+++ routes/users.js
@@ -7,43 +7,48 @@ const User = require('../models/user');
 
 const router = express.Router();
 
-router.get('/register', (req,res) => res.render('register'));
+router.get('/register', (req, res) => res.render('register'));
 
-//function for saving data
+// Login Function
 
-const login = async (serverurl,username, password) =>
-  await axios
-  .post(`${ serverurl }/api/v1/login`, {
-    user: username,
-    password: password,
-  })
-  .then((res) => res.data)
-  .then((res) => {
-	console.log(res);
+const login = async (serverurl, username, password) =>
+	await axios
+	.post(`${ serverurl }/api/v1/login`, {
+		user: username,
+		password: password,
+	})
+	.then((res) => res.data)
+	.then((res) => {
+		console.log(res);
 
-		  return {
-			  status: 'true',
-			  authToken: res.data.authToken,
-			  userId: res.data.userId
-		  };
+		return {
+			status: 'true',
+			authToken: res.data.authToken,
+			userId: res.data.userId
+		};
 
-  })
-  .catch((err) => {
-	console.log(err);
-	return {
-		status: 'false',
-	}
-  });
+	})
+	.catch((err) => {
+		console.log(err);
+		return {
+			status: 'false',
+		}
+	});
 
 
 //Register Handle
 
-router.post('/register', async (req,res) => {
-    const { serverurl, servername, username, password} = req.body;
-	
-	var loginData = await login(serverurl,username,password);
-	
-	if(loginData.status == 'true'){
+router.post('/register', async (req, res) => {
+	const {
+		serverurl,
+		servername,
+		username,
+		password
+	} = req.body;
+
+	var loginData = await login(serverurl, username, password);
+
+	if (loginData.status == 'true') {
 
 		var userid = loginData.userId;
 		var authtoken = loginData.authToken;
@@ -55,32 +60,82 @@ router.post('/register', async (req,res) => {
 			servername,
 			userid,
 			authtoken
-		  });
-		
+		});
+
 		newUser.save()
-		.then(user => console.log(user))
-		.catch(err => console.log(err));
-  
-	
+			.then(user => console.log(user))
+			.catch(err => console.log(err));
+
 		console.log(newUser);
 
-		
-		User.findOne({_id : "842230"})
-		 .then(user =>{
-		 	console.log(user);
-		 })
-		 .catch(err => {
-		 	console.log(err)
-		 })
+		const result = JSON.stringify({
+			code: _id,
+			expiry: 5,
+			status: true
+		})
 
-		res.send(`Data Stored at : ${_id}`);
+		const responseData = JSON.parse(result);
 
-		
+		res.status(200).send(responseData);
+
+
 	} else {
-		res.send('Sorry Authorisation Failed. Please check your data and try again');
+
+		const result = JSON.stringify({
+			error: `Authentication Error`,
+			status: false
+		})
+
+		const responseData = JSON.parse(result);
+
+		res.status(200).send(responseData);
 	}
-    
+
 
 });
+
+router.get('/data', (req, res) => {
+	
+	//console.log(req.query.qcode);
+
+	const qcode = req.query.qcode;
+
+	User.findOne({
+			_id: qcode
+		})
+		.then(data => {
+
+			if(data){
+
+				const result = JSON.stringify({
+					data: data,
+					status: true
+				});
+				const responseData = JSON.parse(result);
+	
+				res.status(200).send(responseData);
+
+			}
+			else {
+
+				const result = JSON.stringify({
+					error: `No Record Found`,
+					status: false
+				})
+	
+				const responseData = JSON.parse(result);
+	
+				res.send(responseData);
+
+			}
+
+		})
+		.catch(err => {
+			console.log(err);
+		});
+
+})
+
+
 
 module.exports = router;
