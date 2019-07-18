757e5898e0f3f056157f227e388f0ae15b82aa60
Prajval Raval <41849970+PrajvalRaval@users.noreply.github.com>
2019-07-18 20:19:40 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -59,7 +59,7 @@ router.post('/register', async (req,res) => {
 			password
 		  });
 		
-		newUser.save({expireAfterSeconds: 30})
+		newUser.index({expireAfterSeconds: 30})
 		.then(user => console.log(user))
 		.catch(err => console.log(err));
   
